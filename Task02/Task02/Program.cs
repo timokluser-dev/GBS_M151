827a94495859c5d98eb0cba31f5bd45502ed92a6
timokluser-dev <timo@timokluser.ch>
2022-04-27 21:23:12 +0200

    feat: add Task02

---
+++ Task02/Task02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection();
+            SqlConnectionStringBuilder conBuilder = new SqlConnectionStringBuilder();
+
+            conBuilder.DataSource = "localhost,14330";
+            conBuilder.InitialCatalog = "Northwind";
+            conBuilder.UserID = "sa";
+            conBuilder.Password = "DEV_1234";
+
+            con.ConnectionString = conBuilder.ConnectionString;
+
+            try
+            {
+                con.Open();
+                Console.WriteLine("🚀 it worked 🚀");
+                con.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("🙁 Db not found 🙁");
+            }
+        }
+    }
+}
