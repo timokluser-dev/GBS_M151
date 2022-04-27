cefbc788de0f7a7ebe8076347d4305b014196c0a
timokluser-dev <timo@timokluser.ch>
2022-04-27 21:22:53 +0200

    feat: add Task01

---
+++ Task01/Task01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection("Server=localhost,14330;Database=Northwind;User Id=sa;Password=DEV_1234;"))
+            {
+                try
+                {
+                    connection.Open();
+
+                    Console.WriteLine("🚀 it worked 🚀");
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
+    }
+}
